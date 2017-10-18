bb2e5d2f131e8f730549a094493b5a7dcafcdf4d
hansyuan <hanzzeh@gmail.com>
2017-10-18 15:24:57 -0700

    rando

--- read.py
+++ read.py
@@ -76,7 +76,7 @@ def difference(base_index, demand_index):
     base_point = bases[base_index]
     (x,y) = (demand_point[0]- base_point[0], demand_point[1] - base_point[1])
     print("Distance: ", (x,y))
-    print("Time: ", times[base_index][demand_index])
+    print("Time: ", times[base_index][demand_index]/60.0)
 
 
 
---
+++ study.py
@@ -0,0 +1,4 @@
+# To get a general sense of the data. Doesn't actually contribute anything.
+
+if __name__ == "__main__":
+    pass
